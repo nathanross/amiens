6261164596b6711414c13330b4d5c69788fabce2
Nathan Ross <nross.se@gmail.com>
2015-12-13 10:32:51 -0500

    add debian dep install script

---
+++ tools/install_dependencies.debian.sh
@@ -0,0 +1,14 @@
+apt-get -y update && apt-get -y install sqlite3 \
+                             python3 \
+                             python-sqlite \
+                             python3-defusedxml \
+                             p7zip \
+                             unzip \
+                             unrar-free \
+                             sox \
+                             libsox-fmt-mp3 \
+                             libav-tools \
+                             flac \
+                             shntool \
+                             vorbis-tools
+
